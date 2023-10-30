73848b70233dc485a4f1468a0a708bbda531e377
Skolens Sk <skolens@skola.madonasvsk.lv>
2023-10-30 10:08:02 +0200

    init

--- 1uzd.py
+++ 1uzd.py
@@ -3,3 +3,35 @@ Uzrakstiet programmu definējot klasi,lai veselu skaitli pārveidotu par romieš
 
 """
 
+class AAA:
+    def __init__(self,):
+        self.roman.numerals={
+             1:"I",
+             4:"IV",
+             5:"V",
+             9:"IX",
+             10:"X",
+             40:"XL",
+             50:"L",
+             90:"XC",
+             100:"C",
+             400:"CD",
+             500:"D",
+             900:"CM",
+             1000:"M",
+
+        }
+      
+    def uz_roman(self, num):
+     result=""#tuksa virkne
+     for value, numeral in sorted(self.roman_numerals.items(), key=lambda x: x[0], reverse=True):
+        while num>= value:
+            result+=numeral
+            num-=value
+     return result
+ 
+    # piemers
+    skaitlis = 1984 #ieddu vertibu
+    convertet= AAA()
+    roman_numeral= convertet.to_roman(skaitlis) # converter- objekts, kuram izsauc metodi- lai parverstu skatli uz romiesu
+    print(f"{skaitlis} romiesu ciparos ir {roman_numeral}")
---
+++ p.py
@@ -0,0 +1,15 @@
+
+#python klases veidošana
+
+class RUDENS:
+ def __init__(self, vards, vecums):
+  self.vards=vards
+  self,vecums=vecums
+  def area(self):
+   print(self.vards, self.vecums)
+
+   #tiek izveidots objekts
+
+   p=RUDENS("ĢInts",25)
+   
+   p.araa()
